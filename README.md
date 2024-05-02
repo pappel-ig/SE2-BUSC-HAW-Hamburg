2ec5b55840adf56c916e57d3ebd209d76e656be7
Jonas <mail@jonasmetzger.de>
2024-05-02 19:11:56 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# SE2 Projekt "Mensa"
+## Rollen
+- *Product Ownerin*: Elizabeth
+- *Scrum Master*: Jonas
+- *Developer*: Sam
+- *Developer*: Jonas
+- *Developer*: Rey
+- *Developer*: Larissa
---
+++ backend/src/main/java/de/haw_hamburg/mensamatch/MensaMatchApplication.java
@@ -0,0 +1,13 @@
+package de.haw_hamburg.mensamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensaMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MensaMatchApplication.class);
+    }
+
+}
