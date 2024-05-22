f4d6c3df13e0fe41412407b96a4e893b8e01b688
Jonas <mail@jonasmetzger.de>
2024-05-22 20:02:45 +0200

    make frontend deployable.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./main.css";
 import axios from 'axios';
 
 if (import.meta.env.DEV) axios.defaults.baseURL = 'http://localhost:8080'
-else axios.defaults.baseURL = 'http://backend:8080'
+else axios.defaults.baseURL = 'http://mensa-match.web.informatik.haw-hamburg.de:8080'
 
 axios.defaults.withCredentials = true;
 
