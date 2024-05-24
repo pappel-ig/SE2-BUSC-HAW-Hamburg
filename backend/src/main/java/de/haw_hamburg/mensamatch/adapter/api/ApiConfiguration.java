0ee32c0abcb9b2daa4ee570b8d4ba56e12332424
Jonas <mail@jonasmetzger.de>
2024-05-24 14:52:02 +0200

    log all requests for further debugging reverse proxy.

--- backend/src/main/java/de/haw_hamburg/mensamatch/adapter/api/ApiConfiguration.java
+++ backend/src/main/java/de/haw_hamburg/mensamatch/adapter/api/ApiConfiguration.java
@@ -2,11 +2,23 @@ package de.haw_hamburg.mensamatch.adapter.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 @Configuration
@@ -23,5 +35,19 @@ public class ApiConfiguration {
     @Bean
     public MappingJackson2HttpMessageConverter converter(ObjectMapper objectMapper) {
         return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    @Order(value = Ordered.HIGHEST_PRECEDENCE)
+    @Component
+    @WebFilter(filterName = "RequestCachingFilter", urlPatterns = "/*")
+    public class RequestCachingFilter extends OncePerRequestFilter {
+
+        private final static Logger LOGGER = LoggerFactory.getLogger(ApiConfiguration.class);
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                        FilterChain filterChain) {
+            LOGGER.info("REQUEST DATA: " + request.getRequestURI());
+        }
     }
 }
