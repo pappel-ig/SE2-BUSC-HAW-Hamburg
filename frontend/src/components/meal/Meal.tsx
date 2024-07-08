757b565ea1ae739af5a6ee6af20510577a3516e4
Jonas <mail@jonasmetzger.de>
2024-07-08 13:16:29 +0200

    meals are represented with 2 floating dezimals.

--- frontend/src/components/meal/Meal.tsx
+++ frontend/src/components/meal/Meal.tsx
@@ -7,6 +7,13 @@ type MealProps = {
 }
 
 export class Meal extends React.Component<MealProps, {}> {
+    formatEur(str: string) {
+        const raw = str.split(" ")
+        const value = raw[1];
+
+        return Number(value).toFixed(2) + " €"
+    }
+
     render() {
         return (
             <li key={this.props.item.name} style={{display: "block"}}>
@@ -15,7 +22,7 @@ export class Meal extends React.Component<MealProps, {}> {
                     ({this.props.item.criteria.join(", ")})
                 </span>
                 <span style={{display: "block", textAlign: "right"}}>
-                    {this.props.item.price.STUD}
+                    {this.formatEur(this.props.item.price.STUD)}
                     <Criteria criteria={this.props.item.criteria}/>
                 </span>
             </li>)
